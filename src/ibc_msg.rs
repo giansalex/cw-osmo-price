8b5025c106f88ec43be336c04a2eeac075adac72
Giancarlos Salas <giansalex@gmail.com>
2022-06-16 14:23:35 -0500

    Update packet

--- src/ibc_msg.rs
+++ src/ibc_msg.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::Binary;
+use cosmwasm_std::{Binary, Decimal, Uint128, Uint64};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -6,10 +6,42 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct PacketMsg {
-    #[serde(skip_serializing_if = "Option::is_none")]
+    /// The unique identifier of this request, as specified by the client
     pub client_id: Option<String>,
-    pub path: String,
-    pub data: Binary,
+    pub query: GammPacket,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum GammPacket {
+    SpotPrice(SpotPricePacket),
+    EstimateSwap(EstimateSwapPacket),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct SpotPricePacket {
+    pub pool: Uint64,
+    pub token_in: String,
+    pub token_out: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct EstimateSwapPacket {
+    pub pool: Uint64,
+    pub sender: String,
+    pub amount: Uint128,
+    pub token_in: String,
+    pub token_out: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct SpotPriceAck {
+    pub price: Decimal,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct EstimateSwapAck {
+    pub amount: Uint128,
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
